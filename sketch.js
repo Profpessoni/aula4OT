2895a4a71ed71c7746d9c0470d7f7a95f0b94c8e
Rodrigo <169179298+Profpessoni@users.noreply.github.com>
2024-05-07 13:21:24 -0300

    Aula 04 Original

---
+++ sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+    createCanvas(600, 400);
+  }
+  
+    function inicializaCores(){
+         
+    background("#165DF3");
+    fill("black");
+    textSize(64);
+    textAlign(CENTER, CENTER)
+      
+    }
+    
+  function draw() {
+    inicializaCores()
+   let maximo = width;
+   let minimo = 0;
+   
+   let palavra = "Caminhante";
+   let quantidade = map(mouseX, 0, width, 0, palavra.length);
+   let parcial = palavra.substring(0,quantidade);
+  
+   text(parcial,300,200); 
+  }
